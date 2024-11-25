7bf3fe6df4e3509fad2f9b66ff4845a5ab59528a
Lorenzo Ferreyra <ferreyralorenzo2@gmail.com>
2024-11-24 22:43:38 -0300

    performance de marcas

--- pages/Marcas.py
+++ pages/Marcas.py
@@ -11,14 +11,14 @@ df = pd.read_excel(path, sheet_name="Marcas", header=None)
 
 # Extract the first three rows for the header
 header_rows = df.iloc[:3]
-
+print(header_rows)
 # Fill forward to deal with merged cells
 header_rows = header_rows.fillna(method="ffill", axis=1)
 print(header_rows)
 # Combine rows to create a single header
 columns = header_rows.apply(lambda x: "_".join(x.dropna().astype(str)), axis=0)
-# Drop the first three rows (headers) from the data
-df = df.iloc[6:]
+
+df = df.iloc[3:]
 
 # Assign the constructed header
 df.columns = columns
@@ -29,7 +29,7 @@ melted_df = df.melt(
     var_name="Country_Metric",
     value_name="Value",
 )
-st.dataframe(melted_df,  hide_index=True)
+
 # Split 'Country_Metric' into 'Country' and 'Metric'
 melted_df[["Country", "Metric"]] = melted_df["Country_Metric"].str.rsplit("_", n=1, expand=True)
 # Asegurarnos de que los valores en Year, Industry y Sector se propaguen hacia abajo
